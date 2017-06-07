78b5aba57007fbbe6b919ebbc68b980f0907298d
Jose Armesto <github@armesto.net>
2017-06-07 09:19:18 +0200

    Port and Debug mode are configurable

--- schip_spinnaker_webhook/__init__.py
+++ schip_spinnaker_webhook/__init__.py
@@ -1,6 +1,8 @@
+import os
+
 from schip_spinnaker_webhook.web import create_app
 
 
 def main():
     app = create_app()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.getenv('PORT', 5000), debug=os.getenv('DEBUG', False))
