27e5b01ea805be2dffbf8483c3c67e910b8a8158
Jose Armesto <github@armesto.net>
2017-06-12 10:24:50 +0200

    Using right type for PORT and DEBUG when starting app

--- schip_spinnaker_webhook/__init__.py
+++ schip_spinnaker_webhook/__init__.py
@@ -5,4 +5,4 @@ from schip_spinnaker_webhook.web import create_app
 
 def main():
     app = create_app()
-    app.run(host="0.0.0.0", port=os.getenv('PORT', 5000), debug=os.getenv('DEBUG', False))
+    app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)), debug=bool(os.getenv('DEBUG', False)))
