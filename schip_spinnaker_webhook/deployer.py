47a16afecd7faafafcdf2391b284e6127907d77c
Øyvind Ingebrigtsen Øvergaard <oyvind.ingebrigtsen.overgaard@finn.no>
2017-06-21 15:57:24 +0200

    DOCD-929 Set fiaas/deployment_id

--- schip_spinnaker_webhook/deployer.py
+++ schip_spinnaker_webhook/deployer.py
@@ -1,18 +1,27 @@
+import uuid
+
 import yaml
 
 from k8s.models.common import ObjectMeta
 from .paasbetaapplication import PaasbetaApplication, PaasbetaApplicationSpec
 
 
+def generate_random_uuid_string():
+    id = uuid.uuid4()
+    return str(id)
+
 class Deployer:
-    def __init__(self, http_client):
+    def __init__(self, http_client, create_deployment_id=generate_random_uuid_string):
         self.http_client = http_client
+        self.create_deployment_id = create_deployment_id
 
     def deploy(self, namespace, release):
         """Create or update TPR for application"""
         application_name = release.application_name
         config = self.download_config(release.config_url)
-        labels = {}
+        labels = {
+            "fiaas/deployment_id": self.create_deployment_id()
+        }
         metadata = ObjectMeta(name=application_name, namespace=namespace, labels=labels)
         spec = PaasbetaApplicationSpec(application=application_name, image=release.image, config=config)
         application = PaasbetaApplication.get_or_create(metadata=metadata, spec=spec)
