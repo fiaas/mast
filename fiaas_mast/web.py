a4d5d0494eca1517f4ec5e492e8121565d4af182
Jose Armesto <github@armesto.net>
2017-09-18 09:17:54 +0200

    The namespace parameter is not mandatory since v1 and v2 can select the namespace in the config file

--- fiaas_mast/web.py
+++ fiaas_mast/web.py
@@ -18,7 +18,7 @@ def health_check():
 @web.route("/deploy/", methods=["PUT", "POST"])
 def deploy_handler():
     data = request.get_json(force=True)
-    required_fields = ("application_name", "config_url", "image", "namespace")
+    required_fields = ("application_name", "config_url", "image")
     errors = ["Missing key {!r} in input".format(key) for key in required_fields if key not in data]
     if errors:
         abort(UnprocessableEntity.code, errors)
