8b4f2541c2e2dc48caaf0b1e0122c0c1e7d659aa
fimojoha <morten.lied.johansen@finn.no>
2017-12-18 09:38:07 +0100

    DOCD-1320 - Support reading Status from both Paasbeta and Fiaas objects

--- fiaas_mast/status.py
+++ fiaas_mast/status.py
@@ -1,8 +1,10 @@
 
 import logging
 
+from k8s.client import NotFound
 from .models import Status
 from .paasbeta import PaasbetaStatus
+from .fiaas import FiaasStatus
 
 
 LOGGER = logging.getLogger(__name__)
@@ -10,10 +12,13 @@ LOGGER = logging.getLogger(__name__)
 
 def status(namespace, application, deployment_id):
     """Get status of a deployment"""
-    search_result = PaasbetaStatus.find(application, namespace, {"fiaas/deployment_id": deployment_id})
-    if not search_result:
-        return Status(status="UNKNOWN", info="No status for {} found".format(deployment_id))
-    if len(search_result) > 1:
-        LOGGER.warning("Found %d status objects for deployment ID %s", len(search_result), deployment_id)
-    status = search_result[-1]
-    return Status(status=status.result, info="Deployment of {} is {}".format(application, status.result.lower()))
+    for model in (PaasbetaStatus, FiaasStatus):
+        try:
+            search_result = model.find(application, namespace, {"fiaas/deployment_id": deployment_id})
+            if len(search_result) > 1:
+                LOGGER.warning("Found %d status objects for deployment ID %s", len(search_result), deployment_id)
+            status = search_result[-1]
+            return Status(status=status.result, info="Deployment of {} is {}".format(application, status.result.lower()))
+        except (NotFound, IndexError):
+            continue
+    return Status(status="UNKNOWN", info="No status for {} found".format(deployment_id))
