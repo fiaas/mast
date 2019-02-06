82a96f15783552d41cc5363cb58ec327a9bd701c
birgirst <birgir.stefansson@schibsted.com>
2019-02-06 12:14:23 +0100

    Add codacy integration

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,29 @@
-FROM alpine:3.6
+FROM python:3.6-alpine3.7 as common
 LABEL maintainer="fiaas <fiaas@googlegroups.com>"
 
-RUN apk add --update ca-certificates git curl tini=0.14.0-r0 python3=3.6.5-r0 && \
+RUN apk add --update ca-certificates git curl tini=0.16.1-r0 && \
     mkdir -p /opt/fiaas-mast && \
     adduser -u 10001 -D -h /opt/fiaas-mast fiaas-mast && \
     rm -rf /var/cache/apk/*
+
+FROM common as build
+
+COPY . /opt/fiaas-mast
+
+WORKDIR /opt/fiaas-mast
+
+RUN pip wheel . --wheel-dir=/wheels/
+
+FROM common as production
+
+COPY --from=build /wheels/ /wheels/
+
+RUN pip install --no-index --find-links=/wheels/ --only-binary all /wheels/fiaas_mast*.whl
+
+USER fiaas-mast
 
 EXPOSE 5000
 
 ENTRYPOINT ["/sbin/tini", "--"]
 
 CMD ["fiaas-mast"]
-
-COPY . /opt/fiaas-mast
-
-WORKDIR /opt/fiaas-mast
-
-RUN pip3 install -r requirements.txt
-
-USER fiaas-mast
--- setup.py
+++ setup.py
@@ -19,6 +19,34 @@ def version():
         warn("Error calling git: {}".format(e))
     return date_string
 
+GEN_REQ = [
+    "Flask==0.12.4",
+    "flask-talisman==0.5.1",
+    "flask-bootstrap==3.3.7.1",
+    "PyYAML==3.12",
+    "requests==2.13.0",
+    "six==1.10.0",
+    "ipaddress==1.0.18",
+    "k8s==0.10.0",
+    "prometheus_client == 0.3.1",
+]
+
+CODE_QUALITY_REQ = [
+    "prospector",
+]
+
+CI_REQ = [
+    "tox==2.7.0",
+    "tox-travis==0.8",
+]
+
+TEST_REQ = [
+    'mock==2.0.0',
+    'pytest-sugar==0.8.0',
+    'pytest==3.0.7',
+    "pytest-cov==2.4.0",
+    "pytest-html==1.14.2",
+]
 
 setup(
     name="fiaas-mast",
@@ -27,23 +55,10 @@ setup(
     maintainer_email="fiaas@googlegroups.com",
     version=version(),
     packages=find_packages(),
-    install_requires=[
-        "Flask==0.12.4",
-        "flask-talisman==0.5.1",
-        "flask-bootstrap==3.3.7.1",
-        "PyYAML==3.12",
-        "requests==2.13.0",
-        "six==1.10.0",
-        "ipaddress==1.0.18",
-        "k8s==0.10.0",
-        "prometheus_client == 0.3.1",
-    ],
+    install_requires=GEN_REQ,
     extras_require={
-        "dev": [
-            'mock==2.0.0',
-            'pytest-sugar==0.8.0', 'pytest==3.0.7', "pytest-cov==2.4.0", "pytest-html==1.14.2", 'yapf==0.16.1',
-            "tox==2.7.0", "tox-travis==0.8", "prospector"
-        ],
+        "dev": ['yapf==0.16.1'] + TEST_REQ + CODE_QUALITY_REQ + CI_REQ,
+        "ci": CI_REQ,
         "codacy": ["codacy-coverage"]
     },
     setup_requires=['setuptools>=17.1', 'pytest-runner', 'wheel'],
